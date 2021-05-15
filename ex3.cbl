74ffc316964ba5228c325794c694dd9d1912f90e
Luan de Oliveira Magalhães <luan.magalhaes@pucpr.edu.br>
2021-05-14 23:06:29 -0300

    OBJETIVO: faça um programa que verifique se a letrada digitada é F OU M conforme a letra escrever, M - MASCULINO F - FEMININO

--- ex3.cbl
+++ ex3.cbl
@@ -1,1 +1,27 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA.
+**************************************
+*******OBJETIVO: faça um programa que verifique se a letrada digitada é F OU M conforme a letra escrever
+***************** M - MASCULINO - F FEMININO
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
 
+       WORKING-STORAGE SECTION.
+       77 WRK-SEXO PIC A(1) VALUES SPACES.
+
+
+       PROCEDURE DIVISION.
+
+       DISPLAY 'INFORME SEU SEXO'.
+       ACCEPT WRK-SEXO FROM CONSOLE.
+
+       EVALUATE WRK-SEXO
+           WHEN 'M'
+               DISPLAY 'MASCULUNO'
+           WHEN 'F'
+               DISPLAY 'FEMININO'
+           WHEN OTHER
+               DISPLAY 'OPCAO INVALIDA'
+       END-EVALUATE.
+
+       STOP RUN.
