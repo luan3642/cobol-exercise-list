cffdd031992e76c65f29bc3f1f9ca23ec3234335
luan3642 <luan3642@hotmail.com>
2021-08-03 18:49:36 -0300

    exs

---
+++ Ex14.cob
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Ex14.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       77 WRK-PESO-PEIXE PIC 9(03) VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+
+       STOP RUN.
